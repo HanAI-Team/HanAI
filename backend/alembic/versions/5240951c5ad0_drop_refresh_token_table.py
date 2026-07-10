f1810f9affedb8db003d085c33448ea7bc9f32e0
YunTaeKyun <98891717+YunTaeKyun93@users.noreply.github.com>
2026-07-10 13:36:12 +0000

    chore: drop unused refresh_token table

---
+++ backend/alembic/versions/5240951c5ad0_drop_refresh_token_table.py
@@ -0,0 +1,28 @@
+"""drop refresh_token table
+
+Revision ID: 5240951c5ad0
+Revises: c59dc4830201
+Create Date: 2026-07-10 13:34:28.596008
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = '5240951c5ad0'
+down_revision: Union[str, None] = 'c59dc4830201'
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+def upgrade() -> None:
+    op.drop_table('refresh_token')
+
+def downgrade() -> None:
+    op.create_table(
+        'refresh_token',
+        sa.Column('id', sa.Integer(), nullable=False),
+        sa.PrimaryKeyConstraint('id')
+    )
